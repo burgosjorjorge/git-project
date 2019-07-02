7e4d7057d94d5680cc608ea0fd16a3084a3bbfb8
burgos <jorgeburgosjr69@gmail.com>
2019-07-02 19:29:01 -0400

    Initial commit

---
+++ bikeshare.py
@@ -0,0 +1,28 @@
+import time
+import pandas as pd
+import numpy as np
+
+
+city = {'chicago': 'chicago.csv', 'new york city': 'new_york_city'}
+
+cities = input('Enter chicago, new york city, or washington ')
+
+def user_input(city):
+    chicago = pd.read_csv('chicago.csv')
+    new_york_city = pd.read_csv('new_york_city.csv')
+    washington = pd.read_csv('washington.csv')
+    chicago['Start Time']= pd.to_datetime(chicago['Start Time'])
+    city = cities
+    ch = chicago['Start Time']
+
+    if city.lower() == 'chicago':
+        return(chicago)
+    elif city.lower() == 'new york city':
+        return(new_york_city)
+    elif city.lower() == 'washington':
+        return(washington)
+    else:
+        print('you entered the wrong city')
+
+print(user_input(city))
+print()
